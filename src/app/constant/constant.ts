a9b78952a41b60a5e68d3b609c31120f3e044bdd
siraz <siraz.odbus@gmail.com>
2026-08-01 14:29:44 +0530

    Extra Seat Block Section Redesing fully

--- src/app/constant/constant.ts
+++ src/app/constant/constant.ts
@@ -20,9 +20,9 @@ export class Constants {
     //  public static PATHURL:any = 'http://127.0.0.1:8000/uploads/';
     //  public static CONSUMER_API_URL:any = 'http://127.0.0.1:8001/api/';
 
-    public static BASE_URL: any = 'https://provider.odbus.co.in/api';
-    public static CONSUMER_API_URL: any = 'https://consumer.odbus.co.in/api/';
-    public static PATHURL: any = 'https://provider.odbus.co.in/public/uploads/';
+    // public static BASE_URL: any = 'https://provider.odbus.co.in/api';
+    // public static CONSUMER_API_URL: any = 'https://consumer.odbus.co.in/api/';
+    // public static PATHURL: any = 'https://provider.odbus.co.in/public/uploads/';
 
     // public static BASE_URL:any ='https://testingadminapi.odbus.co.in/api';
     // public static CONSUMER_API_URL:any ='https://testing.odbus.co.in/api/';
@@ -31,4 +31,9 @@ export class Constants {
     // public static BASE_URL: any = 'http://localhost:7001/ODBUS/odbusproviderbe/api';
     // public static CONSUMER_API_URL: any = 'http://localhost:7001/ODBUS/odbusconsumerbe/api/';
     // public static PATHURL: any = 'http://localhost:7001/ODBUS/odbusproviderbe/public/uploads/';
+
+
+    public static BASE_URL:any ='https://odapi.adglob.in/api';
+    public static CONSUMER_API_URL:any ='https://odclient.adglob.in/api/';
+    public static PATHURL:any = 'https://odapi.adglob.in/public/uploads/';
 }
